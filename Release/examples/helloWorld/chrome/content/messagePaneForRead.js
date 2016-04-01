acfcaa6371a0d4d582b4ad801c0e1c66356dcc24
hechengjin <hechengjin@kingsoft.cn>
2016-04-01 21:10:27 +0800

    添加路径引用相关模块

--- Release/examples/helloWorld/chrome/content/messagePaneForRead.js
+++ Release/examples/helloWorld/chrome/content/messagePaneForRead.js
@@ -1,14 +1,14 @@
-"use strict";
-
+'use strict'
+/*
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cr = Components.results;
 const Cu = Components.utils;
-
-Cu.import("resource://gre/modules/Log.jsm");
-const LOGGER_ID = "addons.manager";
+*/
+const { Log } = require('resource://gre/modules/Log.jsm')
+const LOGGER_ID = 'addons.manager'
 let logger
 window.onload = function () {
-  logger = Log.repository.getLogger(LOGGER_ID);
-  logger.info($(document))  
+  logger = Log.repository.getLogger(LOGGER_ID)
+  logger.info($(document))
 }
--- Release/examples/helloWorld/helloWorld.bat
+++ Release/examples/helloWorld/helloWorld.bat
@@ -1,2 +1,3 @@
+taskkill /f /t /im xulrunner.exe 1>nul 2>&1
 Start "Starting helloWorld" "../../htmlrunner/xulrunner.exe" -app application.ini %*
 exit
