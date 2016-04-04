4ae345a2ea755227841ba758c30acb39f3070654
hechengjin <hechengjin@kingsoft.cn>
2016-04-04 15:37:41 +0800

    增加imap协议测试用例

---
+++ nodejsapps/unitTesting/test_emailjs/imapClient.js
@@ -0,0 +1,42 @@
+﻿"use strict";
+let expect = require('expect.js')
+let ImapClient = require('emailjs-imap-client')
+
+describe('imapClient', function() {
+	let imap
+	before(() => {
+		imap = new ImapClient('imap.139.com', 143, {
+        auth: {
+            user: "15313159857@139.com",
+            pass: "Draeagi94"
+        },
+        useSecureTransport: false,
+        ignoreTLS: true
+    });
+    imap.logLevel = imap.LOG_LEVEL_NONE;
+
+    return imap.connect().then(() => {
+        return imap.selectMailbox('/Inbox');
+    });
+  });
+
+	it('listMailboxes', () => {
+    return imap.listMailboxes().then((mailboxes) => {
+			console.log('listMailboxes result:', mailboxes)
+		});
+  });
+
+	it('selectMailbox', () => {
+    return imap.selectMailbox('INBOX').then((mailbox) => {
+			console.log('selectMailbox result:', mailbox)
+		});
+  });
+
+	after((done) => {
+      imap.close().then(() => {
+				console.log('close OK')
+				done()
+			});
+  });
+
+});
